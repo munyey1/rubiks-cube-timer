b083d78e3932cb536500ead0b39ff39ca411ca8b
Michael Ly <64155853+munyey1@users.noreply.github.com>
2024-08-31 21:37:34 +0100

    Remove old template code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
 <style scoped>
 .logo {
   height: 6em;
@@ -28,3 +12,11 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<script setup>
+import Timer from './components/Timer.vue'
+</script>
+
+<template>
+  <Timer/>
+</template>
