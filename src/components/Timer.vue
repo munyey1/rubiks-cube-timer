3d652010b3087604bf346696eb9d8c23e3555aa7
Michael Ly <michaelly60@outlook.com>
2024-09-04 21:21:21 +0100

    Implement scrambles

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -5,6 +5,8 @@
 </style>
 
 <script>
+import { randomScrambleForEvent } from 'https://cdn.cubing.net/js/cubing/scramble';
+
 export default {
   name: "Timer",
   data() {
@@ -12,6 +14,7 @@ export default {
       startTime: 0,
       elapsedTime: 0,
       timer: null,
+      scramble: '',
       isRunning: false,
       isStopped: false,
       isInspection: true,
@@ -21,7 +24,6 @@ export default {
   methods: {
     start() {
       if (!this.isRunning) {
-        // Use Date.now() to get the current time in milliseconds, more accurate
         this.isStopped = false;
         this.isRunning = true;
         this.startTime = Date.now() - this.elapsedTime * 1000;
@@ -46,15 +48,21 @@ export default {
         }
         if (this.isRunning) {
           this.stop();
-          this.times.push(this.elapsedTime);
+          this.getScramble();
+          this.times.push({time: this.elapsedTime, scramble: this.scramble});
         } else{
           this.start();
         }
       }
     },
+    async getScramble(){
+      const scramble = await randomScrambleForEvent('333');
+      this.scramble = scramble.toString();
+    }
   },
   mounted() {
     window.addEventListener("keyup", this.onUpEvent);
+    this.getScramble();
   },
   beforeUnmount() {
     window.removeEventListener("keyup", this.onUpEvent);
@@ -63,7 +71,8 @@ export default {
 </script>
 
 <template>
-  <h1 class="text-2xl">Timer</h1>
+  <h1 class="text-2xl">Scramble:</h1>
+  <h1 class="text-2xl">{{ scramble }}</h1>
   <h2 class="text-xl">Time: {{ elapsedTime.toFixed(2) }} seconds</h2>
   <div>
     <p>Press the spacebar to start and stop the timer.</p>
@@ -71,6 +80,8 @@ export default {
   <button @click="resetTimer" :disabled="isRunning">Reset</button>
   <h2 v-if="times.length > 0">Times:</h2>
   <ul>
-    <li v-for="(time, index) in times" :key="index">{{ time.toFixed(2) }} seconds</li>
+    <li v-for="(time, index) in times" :key="index">
+      {{ time.time.toFixed(2) }} seconds - {{ time.scramble }}
+    </li>
   </ul>
 </template>
