a8e0b564d00e2d57553c0e9033c1954d605aa7b5
Michael Ly <michaelly60@outlook.com>
2024-09-09 21:27:46 +0100

    remove redundant code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
 
 <script setup>
 import { onMounted, ref } from "vue";
 import { supabase } from "./supabase";
 
-import Account from "./components/Account.vue";
 import Auth from "./components/Auth.vue";
 import Timer from "./components/Timer.vue";
 
@@ -31,17 +17,17 @@ onMounted(() => {
     session.value = _session;
   });
 });
-
-// https://supabase.com/docs/reference/javascript/auth-signinwithoauth use oauth instead
-// will use google, and there should be an automatic verification
-// i assume this is creating a session for the user, so the user will just get redirected to the timer page
-// i'll check if the timer loads up correctly through oauth, but if i need another page for the timer then so be it
-
 </script>
 
 <template>
-  <div v-if="session" className="font-mono container min-w-full max-h-full flex flex-col items-center">
-    <div role="tablist" className="container min-w-full mt-6 tabs tabs-bordered">
+  <div
+    v-if="session"
+    className="font-mono container min-w-full max-h-full flex flex-col items-center"
+  >
+    <div
+      role="tablist"
+      className="container min-w-full mt-6 tabs tabs-bordered"
+    >
       <input
         type="radio"
         name="my_tabs_1"
@@ -64,8 +50,7 @@ onMounted(() => {
       <div role="tabpanel" className="tab-content p-10"></div>
     </div>
   </div>
-  <div v-else className="font-mono container flex flex-col items-center" >
+  <div v-else className="font-mono container flex flex-col items-center">
     <Auth />
   </div>
 </template>
-
