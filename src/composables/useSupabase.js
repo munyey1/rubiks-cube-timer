9f7a5c8e962f0329cdddbdb73182138abe23b340
Michael Ly <64155853+munyey1@users.noreply.github.com>
2024-09-19 19:56:08 +0100

    refactor supabase call

--- src/composables/useSupabase.js
+++ src/composables/useSupabase.js
@@ -1,19 +1,14 @@
 import { supabase } from "../supabase";
 import { ref } from "vue";
 
-export function GetSolves(session) {
-  const times = ref([]);
-
-  const getTimes = async () => {
-    const { data, error } = await supabase
-      .from("solves")
-      .select("*")
-      .eq("user_id", session.user.id);
-    if (error) {
-      console.error(error);
-    } else{
-      times.value = data;
-    }
-  };
-  return { times, getTimes };
+export async function GetTimes(userId) {
+  const { data, error } = await supabase
+    .from("solves")
+    .select("*")
+    .eq("user_id", userId);
+  if (error) {
+    console.error(error);
+    return [];
+  }
+  return data;
 }
