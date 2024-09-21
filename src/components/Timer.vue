d900bfb62bd3fc97cad8980a1eebd10bdbaca3b3
Michael Ly <64155853+munyey1@users.noreply.github.com>
2024-09-21 20:08:29 +0100

    remove redundant code and use composable

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -18,6 +18,8 @@
 import { randomScrambleForEvent } from "https://cdn.cubing.net/js/cubing/scramble";
 import { supabase } from "../supabase";
 
+import { calculateAverage } from "../composables/calcAvg";
+
 export default {
   props: {
     session: Object,
@@ -170,51 +172,7 @@ export default {
       }
     },
     calculateAverage(num) {
-      if (this.times.length < num) {
-        return "N/A";
-      } else {
-        // Get the last n times
-        // Remove the max and min times
-        // Calculate the average of the remaining times
-        const times = this.times.slice(-num);
-        // Get the max time
-        // Add plus 2 and dnf checking
-        const max = Math.max(
-          ...times.map((time) => {
-            if (time.time.includes("(+)")) {
-              return time.time.replace("(+)", "");
-            }
-            if (time.time == "DNF") {
-              return 0;
-            }
-            return time.time;
-          })
-        );
-        // Get the minimum time
-        const min = Math.min(
-          ...times.map((time) => {
-            if (time.time.includes("(+)")) {
-              return time.time.replace("(+)", "");
-            }
-            if (time.time == "DNF") {
-              return 0;
-            }
-            return time.time;
-          })
-        );
-        const sum = times.reduce((acc, time) => {
-          if (time.time.includes("(+)")) {
-            const plus2 = time.time.replace("(+)", "");
-            return acc + Number(plus2);
-          }
-          if (time.time == "DNF") {
-            return acc;
-          }
-          return acc + Number(time.time);
-        }, 0);
-        const average = ((sum - max - min) / (times.length - 2)).toFixed(2);
-        return average;
-      }
+      return calculateAverage(num, this.times);
     },
   },
   mounted() {
