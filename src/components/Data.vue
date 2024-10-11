7e3d40caa490a397a1258fcebfc336f314816f70
Michael Ly <64155853+munyey1@users.noreply.github.com>
2024-10-11 19:24:35 +0100

    change to script setup

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -5,20 +5,16 @@
     <div className="container col-span-2 flex flex-col items-center">
       <p>Best Time: {{ bestTime() }} seconds</p>
       <p>Worst Time: {{ worstTime() }} seconds</p>
-      <LineChart
-        :data="lineData"
-        :options="options"
-        className="ml-4"
-      />
+      <Line :data="lineData" :options="options" className="ml-4" />
       <button className="btn btn-outline mt-4" @click="updateChart">
         Update Chart
       </button>
     </div>
 
     <div className="container ml-10">
-      <p>Average of last 5: {{ calculateAverage(5) }}</p>
-      <p>Average of last 12: {{ calculateAverage(12) }}</p>
-      <p>Average of last 50: {{ calculateAverage(50) }}</p>
+      <p>Average of last 5: {{ getAverage(5) }}</p>
+      <p>Average of last 12: {{ getAverage(12) }}</p>
+      <p>Average of last 50: {{ getAverage(50) }}</p>
       <h2 className="text-lg mt-10 ">Times:</h2>
       <div className="mr-20 overflow-y-scroll max-h-80">
         <ol className="list-decimal list-inside">
@@ -32,9 +28,9 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue";
 import { calculateAverage } from "../composables/calcAvg";
-import LineChart from "./LineChart.vue";
 
 import {
   Chart as ChartJS,
@@ -47,7 +43,6 @@ import {
   PointElement,
   LineElement,
 } from "chart.js";
-import { ref } from "vue";
 import { Line } from "vue-chartjs";
 
 ChartJS.register(
@@ -61,147 +56,108 @@ ChartJS.register(
   Legend
 );
 
-export default {
-  props: {
-    session: Object,
-    times: Array,
-  },
-  components: {
-    Line,
-    LineChart,
-  },
-  data() {
-    const parseTime = (time) => {
-      if (time.includes("(+)")) {
-        return time.replace("(+)", "");
-      }
-      if (time === "DNF") {
-        return 0;
-      }
-      return time;
-    };
+const props = defineProps({
+  session: Object,
+  times: Array,
+});
 
-    const labels = this.times.map((time) => {
-      const date = new Date(time.solved_at);
-      return (
-        date.getHours() +
-        ":" +
-        date.getMinutes() +
-        " " +
-        date.getDate() +
-        "/" +
-        date.getMonth() +
-        "/" +
-        date.getFullYear()
-      );
-    });
+const parseTime = (time) => {
+  if (time.includes("(+)")) {
+    return time.replace("(+)", "");
+  }
+  if (time === "DNF") {
+    return 0;
+  }
+  return time;
+};
 
-    const data = this.times.map((time) => parseTime(time.time));
-    return {
-      lineData: {
-        labels: labels,
-        datasets: [
-          {
-            data: data,
-            backgroundColor: "#FFFFFF",
-            borderColor: "#FF0000",
-          },
-        ],
+const labels = computed(() => {
+  return props.times.map((time) => {
+    const date = new Date(time.solved_at);
+    return (
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      " " +
+      date.getDate() +
+      "/" +
+      date.getMonth() +
+      "/" +
+      date.getFullYear()
+    );
+  });
+});
+
+const data = computed(() => {
+  return props.times.map((time) => parseTime(time.time));
+});
+
+const lineData = computed(() => ({
+  labels: labels.value,
+  datasets: [
+    {
+      data: data.value,
+      backgroundColor: "#FFFFFF",
+      borderColor: "#FF0000",
+    },
+  ],
+}));
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+// Methods
+const getAverage = (num) => {
+  return calculateAverage(num, props.times);
+};
+
+const bestTime = () => {
+  const parsedTimes = props.times
+    .map((time) => parseTime(time.time))
+    .filter((t) => t > 0);
+  const best = Math.min(...parsedTimes);
+  return best === Infinity ? null : best;
+};
+
+const worstTime = () => {
+  const parsedTimes = props.times.map((time) => parseTime(time.time));
+  const worst = Math.max(...parsedTimes);
+  return worst === -Infinity ? null : worst;
+};
+
+const updateChart = () => {
+  const labels = props.times.map((time) => {
+    const date = new Date(time.solved_at);
+    return (
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      " " +
+      date.getDate() +
+      "/" +
+      date.getMonth() +
+      "/" +
+      date.getFullYear()
+    );
+  });
+
+  const data = props.times.map((time) => parseTime(time.time));
+
+  lineData.value = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        backgroundColor: "#FFFFFF",
+        borderColor: "#FF0000",
       },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            display: false,
-          },
-        },
-      },
-      newlabel: labels,
-      newdata: data,
-    };
-  },
-  methods: {
-    calculateAverage(num) {
-      return calculateAverage(num, this.times);
-    },
-    bestTime() {
-      const parseTime = (time) => {
-        if (time.includes("(+)")) {
-          return Number(time.replace("(+)", ""));
-        } else if (time === "DNF") {
-          return Infinity;
-        }
-        return time;
-      };
-      const times = this.times.map((time) => parseTime(time.time));
-
-      const bestTime = Math.min(...times);
-
-      return bestTime === Infinity ? null : bestTime;
-    },
-    worstTime() {
-      const parseTime = (time) => {
-        if (time.includes("(+)")) {
-          return Number(time.replace("(+)", ""));
-        } else if (time === "DNF") {
-          return -Infinity;
-        }
-        return time;
-      };
-      const times = this.times.map((time) => parseTime(time.time));
-
-      const worstTime = Math.max(...times);
-      return worstTime === -Infinity ? null : worstTime;
-    },
-    test() {
-      const date = new Date(Date.now()).toISOString();
-      console.log(date);
-      const list = this.times.map((time) => console.log(time.time));
-      console.log(this.times);
-    },
-    updateChart() {
-      const parseTime = (time) => {
-        if (time.includes("(+)")) {
-          return time.replace("(+)", "");
-        }
-        if (time === "DNF") {
-          return 0;
-        }
-        return time;
-      };
-
-      const labelss = this.times.map((time) => {
-        const date = new Date(time.solved_at);
-        return (
-          date.getHours() +
-          ":" +
-          date.getMinutes() +
-          " " +
-          date.getDate() +
-          "/" +
-          date.getMonth() +
-          "/" +
-          date.getFullYear()
-        );
-      });
-
-      const datas = this.times.map((time) => parseTime(time.time));
-
-      this.lineData.labels = {
-        labels: labelss,
-        datasets: [
-          ...this.lineData.datasets,
-          {
-            data: datas,
-            backgroundColor: "#FFFFFF",
-            borderColor: "#FF0000",
-          },
-        ],
-      };
-    },
-  },
-  mounted() {
-    this.updateChart();
-  },
+    ],
+  };
 };
 </script>
