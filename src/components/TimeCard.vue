4f8def32e2faaa8a85c45813a7687c357c6929d0
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-02-21 21:27:08 +0000

    modal testing

--- src/components/TimeCard.vue
+++ src/components/TimeCard.vue
@@ -1,20 +1,47 @@
 <script setup>
+import { ref } from "vue";
 
 const props = defineProps({
   time: Object,
 });
 
+const modalRef = ref(null);
+
+const showModal = ref(false);
+
+const toggleModal = () => {
+  showModal.value = !showModal.value;
+  console.log(showModal.value);
+};
+
 </script>
 
 <template>
-  <div>
+  <div v-if="props.time">
     <div class="card">
       <div class="card-content">
-        <p class="title is-4">{{ time.time }}</p>
-        <p class="subtitle is-6">{{ time.scramble }}</p>
-        <p class="subtitle is-6">{{ time.solved_at }}</p>
+        <p>{{ time.time }} seconds</p>
+        <p>Scramble - {{ time.scramble }}</p>
       </div>
     </div>
+    <button class="btn" @click="toggleModal">More Details</button>
+
+    <dialog ref="modalRef" v-if="showModal" class="modal">
+      <div class="modal-box">
+        <h3 class="text-lg font-bold">{{ time.time }} seconds</h3>
+        <p class="py-4">Press ESC key or click outside to close</p>
+        <p>Solved at - {{ time.solved_at }}</p>
+        <twisty-player
+          background="none"
+          controlPanel="none"
+          visualization="2D"
+          :alg="time.scramble"
+        ></twisty-player>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button @click="toggleModal">close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
