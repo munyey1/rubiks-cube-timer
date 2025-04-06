237025fc189b69b8911316a85131b649dd7a75c0
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-04-06 18:26:28 +0100

    uninstall vue3-google-login

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import vue3GoogleLogin from "vue3-google-login";
 
-const app = createApp(App);
-
-app.use(vue3GoogleLogin, {
-  clientId: `783993305941-hft8a0d5afmg9fshm7hic5ulsutccmdj.apps.googleusercontent.com`,
-});
-
-app.mount("#app");
+createApp(App).mount("#app");
