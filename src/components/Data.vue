66c747549dc3dcdb46edaca9c83a82f4943da216
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-04-07 18:09:04 +0100

    use LineFilter.vue

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from "vue";
 
-import TimeList from "./TimeList.vue";
+import LineFilter from "./charts/LineFilter.vue";
 import LineChart from "./charts/LineChart.vue";
 import DoughnutChart from "./charts/DoughnutChart.vue";
 import SummaryStats from "./SummaryStats.vue";
+import TimeList from "./TimeList.vue";
 
 const props = defineProps({
   session: Object,
@@ -17,18 +18,7 @@ const lineFilter = ref(0);
 <template>
   <div className="container min-w-full grid lg:grid-cols-3 sm:grid-cols-1">
     <div className="container lg:col-span-2 flex flex-col items-center">
-      <p>Select Filter</p>
-      <div>
-        <select v-model="lineFilter">
-          <option value="" selected disabled>Please select</option>
-          <option value="0">All</option>
-          <option value="5">Last 5</option>
-          <option value="12">Last 12</option>
-          <option value="25">Last 25</option>
-          <option value="50">Last 50</option>
-          <option value="100">Last 100</option>
-        </select>
-      </div>
+      <LineFilter v-model="lineFilter" className="mt-2" />
       <LineChart
         :times="props.times"
         :lineFilter="Number(lineFilter)"
@@ -44,7 +34,7 @@ const lineFilter = ref(0);
 
     <div className="flex">
       <div className="lg:fixed p-6">
-        <SummaryStats :times="props.times" className="mt-4" />
+        <SummaryStats className="mt-4" :times="props.times" />
         <h2 className="text-lg mt-6">Times:</h2>
         <TimeList className="overflow-y-auto h-96" :times="times" />
       </div>
