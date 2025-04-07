94c801cf3dd25e66849384d35c136dfc35ab7e9f
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-04-07 18:08:57 +0100

    Update LineFilter.vue

--- src/components/charts/LineFilter.vue
+++ src/components/charts/LineFilter.vue
@@ -1,7 +1,18 @@
 <script setup>
-
+const model = defineModel();
 </script>
 
 <template>
-
+  <div>
+    <p>Select Filter</p>
+    <select v-model="model">
+      <option value="" selected disabled>Please select</option>
+      <option value="0">All</option>
+      <option value="5">Last 5</option>
+      <option value="12">Last 12</option>
+      <option value="25">Last 25</option>
+      <option value="50">Last 50</option>
+      <option value="100">Last 100</option>
+    </select>
+  </div>
 </template>
