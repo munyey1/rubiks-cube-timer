50b74e77b2edcc098198b6a6b2ba7a6b3017d8a8
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-04-17 00:15:43 +0100

    Remove unused logout function

--- src/App.vue
+++ src/App.vue
@@ -20,17 +20,6 @@ onMounted(() => {
   });
 });
 
-const logout = async () => {
-  if (!session.value) {
-    console.warn("No session to log out from.");
-    return;
-  }
-
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.error("Error logging out:", error.message);
-  }
-};
 </script>
 
 <template>
